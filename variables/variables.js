7479714e581be1428e0b56ca9bd48ae79b65f2ad
FarhanAfrad1 <farhan.tanmoy13@gmail.com>
2025-01-12 15:18:50 +0600

    variables

---
+++ variables/variables.js
@@ -0,0 +1,15 @@
+const accountId = "123233"
+let accountPassword = "a234a"
+var accountCity = "Dhaka"
+
+// accountId = "1111" //this is not valid as accountId is declared as constant.
+
+/*
+var is not used anymore because var is not compatible with block scope and 
+funtion scope.
+*/
+
+// to declare a variable either used const or let
+
+console.log(accountId)
+console.table([accountId,accountPassword,accountCity])
